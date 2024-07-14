92246e19107a61908da40917913b3341d168187d
sohuigim <aisurudake@gmail.com>
2024-07-14 22:37:45 +0900

    0714

--- ThisJava/src/ch07/sec02/SmartPhone.java
+++ ThisJava/src/ch07/sec02/SmartPhone.java
@@ -6,7 +6,13 @@ public class SmartPhone extends Phone {
 	
 	//생성자 선언
 	public SmartPhone(String model, String color) {
-		this.model;
+		this.model = model;
+		this.color = color;
 	}
-
+	
+	//메소드 선언
+	public void setWifi(boolean wifi) {
+		this.wifi = wifi;
+		System.out.println("와이파이 상태를 변경했습니다.");
+	}
 }
